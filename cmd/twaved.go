cd3c65d51108db8542bac425f60fb38e68e900f6
Mark Pashby <markpashby@gmail.com>
2015-02-16 21:01:42 +0000

    added in CacheFave function to begin caching results

--- cmd/twaved.go
+++ cmd/twaved.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
+	"github.com/ChimeraCoder/anaconda"
 	"net/url"
+	"time"
 )
+
+func CacheFaves(twapi *anaconda.TwitterApi) {
+
+	faveStringValues := url.Values{}
+	faveStringValues.Set("count", "20")
+	faveGetResults, _ := twapi.GetFavorites(faveStringValues)
+	for _, tweet := range faveGetResults {
+		fmt.Println(tweet.Text, tweet.Id)
+	}
+}
 
 func main() {
 	// file contains twitter api keys
 	const keyFile = "config.json"
-	// Search String Additional Values
-	faveStringValues := url.Values{}
-	faveStringValues.Set("count", "20")
+	// API max call limit on 15 min reset
+	const apiCallLimit = 15
 	apiKeys := LoadAuthKeys(keyFile)
 	twApi := PerformAuth(apiKeys)
-	faveGetResults, _ := twApi.GetFavorites(faveStringValues)
-	for _, tweet := range faveGetResults {
-		fmt.Println(tweet.Text, tweet.Id)
-	}
+	CacheFaves(twApi)
 }
